d8e89676883dc0e85aa3759b0ae8609747dc0bd0
Rafael Viscarra <rafaelviscarra@gmail.com>
2016-04-10 12:54:23 -0600

    Added json dep.

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule FtChat.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-        {:cowboy, "~> 1.0"}
+        {:cowboy, "~> 1.0"},
+        {:json, "~> 0.3.0"}
     ]
   end
 end
