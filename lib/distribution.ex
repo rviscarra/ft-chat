692ada3c66a41f99a94b8a6f204ad2284fc7970c
Rafael Viscarra <rafaelviscarra@gmail.com>
2016-04-10 12:31:56 -0600

    Virtual node ring

---
+++ lib/distribution.ex
@@ -0,0 +1,30 @@
+defmodule FtChat.Distribution do
+
+    defmodule NodeRing do
+        defstruct [:rules, :replication, :partitions]
+
+        def new(nodes, replication, partitions \\ 32) do
+            num_nodes = Enum.count nodes
+            ring =
+                1..partitions
+                |> Enum.map(fn i ->
+                    Enum.at nodes, (rem(i - 1, num_nodes))
+                end)
+            %HashRing {
+                rules: List.to_tuple(ring),
+                replication: replication,
+                partitions: partitions
+            }
+        end
+
+        def get_node_for(ring, value) do
+            hash = :erlang.phash2 value
+            lower_index = rem(hash, ring.partitions)
+            upper_index = lower_index + ring.replication - 1
+            lower_index..upper_index
+            |> Enum.map(&(elem ring.rules, rem(&1, ring.partitions)))
+        end
+
+    end
+    
+end
