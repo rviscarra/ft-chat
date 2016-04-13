2fb4708091dfa741d5f6be321c2c98b2a832ffc5
Rafael Viscarra <rafaelviscarra@gmail.com>
2016-04-12 22:53:18 -0600

    Local cluster script improvements

--- run_local_cluster.sh
+++ run_local_cluster.sh
@@ -6,8 +6,10 @@ function cleanup {
 }
 
 trap cleanup SIGINT SIGTERM EXIT
+mix compile
 
-elixir --cookie pmfVB5bT1tw2pIjWflzph --sname node1@localhost -S mix run_node --port 8000 --no-halt & 
-elixir --cookie pmfVB5bT1tw2pIjWflzph --sname node2@localhost -S mix run_node --port 8001 --no-halt & 
-iex --cookie pmfVB5bT1tw2pIjWflzph --sname node3@localhost -S mix run_node --port 8002
-
+tmux -L ft_chat new-session -d "elixir --cookie pmfVB5bT1tw2pIjWflzph --sname node1@localhost -S mix run_node --port 8000 --no-halt"
+tmux -L ft_chat split-window "elixir --cookie pmfVB5bT1tw2pIjWflzph --sname node2@localhost -S mix run_node --port 8001 --no-halt"
+tmux -L ft_chat split-window "iex --cookie pmfVB5bT1tw2pIjWflzph --sname node3@localhost -S mix run_node --port 8002"
+tmux -L ft_chat select-layout even-vertical
+tmux -L ft_chat attach
